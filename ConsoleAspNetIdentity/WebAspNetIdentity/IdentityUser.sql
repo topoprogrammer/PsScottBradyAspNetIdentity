1070f14715e984c72ec9b9aa34d7d4cfc0b10191
DESKTOP-NDGE3EJ\CarlosLap <condesa.sys@gmail.com>
2019-02-21 17:50:59 -0600

    Identity test user password added.

--- ConsoleAspNetIdentity/WebAspNetIdentity/IdentityUser.sql
+++ ConsoleAspNetIdentity/WebAspNetIdentity/IdentityUser.sql
@@ -3,3 +3,6 @@ GO
 INSERT [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
 VALUES (N'caa43f61-3cf2-4e53-a3ad-43f0fb3150bb', N'topoprogrammer@gmail.com', 1, N'AGNcL+XL3KEwt0H0LO+BeKMkk083CMovX/ia6BFrXtvQ9r+03mKTPHg1SPQ693oxBQ==', N'c6a365bd-a1b4-4437-a37b-112b4eb02485', N'+', 1, 0, NULL, 1, 0, N'topoprogrammer@gmail.com')
 GO
+
+--Password:
+--Password1@
